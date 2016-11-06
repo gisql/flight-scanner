448e91a522edd1f5f76a533a0ba90538fa217cb1
Michal Ostapowicz <michal@clearscore.com>
2016-11-06 10:20:55 +0000

    Fixed termination and removed OPO and STN from searches

--- src/main/scala/dev/mo/flights/Main.scala
+++ src/main/scala/dev/mo/flights/Main.scala
@@ -5,8 +5,10 @@ import java.time.temporal.ChronoUnit
 
 import akka.actor.ActorSystem
 
+import scala.concurrent.Future
+
 object Main extends App with Scanner {
-  override implicit def system = ActorSystem("Scanner")
+  override implicit lazy val system = ActorSystem("Scanner")
   /*
     private val destinations = List("SZG", "PFO", "PRG", "SXF", "HAM", "BRE", "ATH", "KGS", "RHO", "BUD", "DUB", "BGY",
       "MXP", "GOA", "BLQ", "PSA", "FCO", "CIA", "TSF", "VCE", "VRN", "LUX", "MLA", "EIN", "RYG", "TRF", "OSL", "GDN",
@@ -22,16 +24,27 @@ object Main extends App with Scanner {
     )
   */
 
-  def fire(w: Int) = oneDayTrips(LocalDate.now().plus(w, ChronoUnit.WEEKS),
-    "STN", "SZG", "PFO", "PRG", "SXF", "BRE", "ATH", "KGS", "RHO", "BUD",
+  def fire(w: Int) = oneDayTrips(LocalDate.of(2017, 3, 3).plus(w, ChronoUnit.WEEKS),
+    "SZG", "PFO", "PRG", "SXF", "BRE", "ATH", "KGS", "RHO", "BUD",
     "GOA", "BLQ", "PSA", "FCO", "CIA", "VCE", "VRN", "LUX", "MLA", "EIN", "RYG", "TRF", "OSL", "GDN",
-    "OPO", "LIS", "IBZ", "MAD", "AGP", "PMI", "SVQ", "VLC", "NYO", "EDI").map(trips =>
+    "LIS", "IBZ", "MAD", "AGP", "PMI", "SVQ", "VLC", "NYO", "EDI").map(trips =>
     trips.sortBy(_.price).foreach(t =>
       println(f"${t.to},${t.price}%2.2f,${t.outTime.toLocalDate},${t.outTime.toLocalTime},${t.backTime.toLocalTime}"))
   )
 
   println("Flights from STN")
-  fire(35).andThen { case x =>
-    println(s"Finished and then: $x")
+  def retry(n: Int, cmd: => Future[_]): Future[_] = cmd recoverWith {
+    case e =>
+      if (n <= 0) throw e
+      else {
+        println(s"Retrying $n: $e")
+        retry(n - 1, cmd)
+      }
+  }
+
+  retry(5, fire(0)) onComplete {
+    _ =>
+      println("terminating...")
+      system.terminate()
   }
 }
